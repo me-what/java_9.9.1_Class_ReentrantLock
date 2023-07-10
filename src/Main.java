250ec21a7124551089cdb4a843ec820131c8870d
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-07-10 21:42:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    public static void main(String[] args) {
+        CommonResource commonResource = new CommonResource();
+        ReentrantLock locker = new ReentrantLock(); // создаем заглушку
+        for (int i = 1; i < 6; i++) {
+
+            Thread t = new Thread(new CountThread(commonResource, locker));
+            t.setName("Thread " + i);
+            t.start();
+        }
+    }
+}
